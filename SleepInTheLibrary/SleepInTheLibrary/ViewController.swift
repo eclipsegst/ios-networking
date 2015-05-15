a21188e9fd4ed4e21cdc7ba3d32820d01c40077b
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-05-15 16:54:45 -0500

    initial commit

---
+++ SleepInTheLibrary/SleepInTheLibrary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SleepInTheLibrary
+//
+//  Created by iOS Students on 5/15/15.
+//  Copyright (c) 2015 Zhaolong Zhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
