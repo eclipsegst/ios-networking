0692d81ff97cfed289d9e81cd1c0702590ff0851
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-05-17 23:00:01 -0500

    completed search by latitude and longitude

--- FlickFinder/FlickFinder/ViewController.swift
+++ FlickFinder/FlickFinder/ViewController.swift
@@ -18,6 +18,13 @@ class ViewController: UIViewController {
     let DATA_FORMAT = "json"
     let NO_JSON_CALLBACK = "1"
     
+    let BOUNDING_BOX_HALF_WIDTH = 1.0
+    let BOUNDING_BOX_HALF_HEIGHT = 1.0
+    let LAT_MIN = -90.0
+    let LAT_MAX = 90.0
+    let LON_MIN = -180.0
+    let LON_MAX = 180.0
+    
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var photoTitleLabel: UILabel!
     @IBOutlet weak var defaultLabel: UILabel!
@@ -28,6 +35,9 @@ class ViewController: UIViewController {
     var tapRecognizer: UITapGestureRecognizer? = nil
 
     @IBAction func searchPhotosByPhraseButtonTouchUp(sender: AnyObject) {
+        
+        // Hide keyboard after searching
+        self.dismissAnyVisibleKeyboards()
         
         let methodArguments = [
             "method": METHOD_NAME,
@@ -46,6 +56,19 @@ class ViewController: UIViewController {
     
     @IBAction func searchPhotosByLatLonButtonTouchUp(sender: AnyObject) {
         
+        self.dismissAnyVisibleKeyboards()
+        
+        let methodArguments = [
+            "method": METHOD_NAME,
+            "api_key": API_KEY,
+            "bbox": createBoundingBoxString(),
+            "safe_search": SAFE_SEARCH,
+            "extras": EXTRAS,
+            "format": DATA_FORMAT,
+            "nojsoncallback": NO_JSON_CALLBACK
+        ]
+        
+        getImageFromFlickBySearch(methodArguments)
     }
     
     override func viewDidLoad() {
@@ -127,7 +150,18 @@ class ViewController: UIViewController {
         return keyboardSize.CGRectValue().height
     }
     
-    
+    func createBoundingBoxString() -> String {
+        
+        let latitude = (self.latitudeTextField.text as NSString).doubleValue
+        let longitude = (self.longitudeTextField.text as NSString).doubleValue
+        
+        let bottom_left_lon = max(longitude - BOUNDING_BOX_HALF_WIDTH, LON_MIN)
+        let bottom_left_lat = max(latitude - BOUNDING_BOX_HALF_HEIGHT, LAT_MIN)
+        let top_right_lon = min(longitude + BOUNDING_BOX_HALF_HEIGHT, LON_MAX)
+        let top_right_lat = min(latitude + BOUNDING_BOX_HALF_HEIGHT, LAT_MAX)
+        
+        return "\(bottom_left_lon),\(bottom_left_lat),\(top_right_lon),\(top_right_lat)"
+    }
  
     func getImageFromFlickBySearch(methodArguments: [String : AnyObject]) {
     
@@ -228,4 +262,13 @@ class ViewController: UIViewController {
         return (!urlVars.isEmpty ? "?" : "") + join("&", urlVars)
     }
 }
+
+extension ViewController {
+    func dismissAnyVisibleKeyboards() {
+        if phraseTextField.isFirstResponder() || latitudeTextField.isFirstResponder() || longitudeTextField.isFirstResponder() {
+            self.view.endEditing(true)
+        }
+    }
+}
+
 
